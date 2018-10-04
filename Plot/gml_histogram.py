899401277d7a45fca75d68329e41420f5e7a1884
Ricardo <ricagraca@hotmail.com>
2018-10-04 19:42:37 +0100

    Added a py program 'gml_histogram.py' that takes a gml file and plots a histogram

---
+++ Plot/gml_histogram.py
@@ -0,0 +1,35 @@
+import networkx as nx
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.stats as ss
+import pylab
+import sys
+
+# Input 1 -> GML file
+
+if __name__ == '__main__':
+
+	# Load graph using gml format
+	file_name = sys.argv[1]
+	G = nx.read_gml(file_name,True)
+
+	# Get an histogram degree
+	kv = [k for (v,k) in G.degree()]
+
+	# Change window title
+	fig = pylab.gcf()
+	fig.canvas.set_window_title('Degree Distribution')
+
+	# Set graph labels
+	file_path_splitted = file_name.split("/")
+	number_of_directories = len(file_path_splitted)
+	plt.title('Network Graph: ' + file_path_splitted[number_of_directories-1].split('.')[0].upper())
+	plt.xlabel('Degree of vertexes')
+	plt.ylabel('Number of vertexes')
+
+	# Set histogram
+	kmax = max(kv)
+	plt.hist(kv, bins=kmax+1, range=(0,kmax+1))
+
+	# Show histogram
+	plt.show()
