5ca2b8cfed37898110c37352ac3cea44a71f8819
Ricardo <ricagraca@hotmail.com>
2018-10-05 21:07:47 +0100

    Added graph to the histogram

--- Plot/gml_histogram.py
+++ Plot/gml_histogram.py
@@ -23,13 +23,21 @@ if __name__ == '__main__':
 	# Set graph labels
 	file_path_splitted = file_name.split("/")
 	number_of_directories = len(file_path_splitted)
+
+	# Set histogram
+	kmax = max(kv)
+        plt.subplot(1, 2, 1)
+        plt.hist(kv, bins=kmax+1, range=(0,kmax+1), normed=True)
 	plt.title('Network Graph: ' + file_path_splitted[number_of_directories-1].split('.')[0].upper())
 	plt.xlabel('Degree of vertexes')
 	plt.ylabel('Number of vertexes')
 
-	# Set histogram
-	kmax = max(kv)
-	plt.hist(kv, bins=kmax+1, range=(0,kmax+1), normed=True)
-
-	# Show histogram
-	plt.show()
+	# Draw graph
+        plt.subplot(1, 2, 2)
+        color_map = [(0,1.-float(v)/float(kmax),0) for v in kv] # Set color for each vertex
+        pos = nx.spring_layout(G,k=0.15,iterations=20)
+        nx.draw(G,node_color=color_map, pos=pos)
+        
+	# Show
+        plt.show()
+        
