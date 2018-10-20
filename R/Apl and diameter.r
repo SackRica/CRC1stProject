00fcdbab8f43da562ddaf2816aef326920b272e7
dianamlopes22 <31066715+dianamlopes22@users.noreply.github.com>
2018-10-20 19:25:56 +0100

    Update Apl and diameter.r

--- R/Apl and diameter.r
+++ R/Apl and diameter.r
@@ -1,1 +1,16 @@
+library (igraph)
+library(poweRlaw)
+library(data.table)
 
+f <- read.table(file="../fb_edgeList.txt")
+edges <- c()
+edges <- as.vector(t(f[,1:2]))
+for (e in 1:dim(f)[1]){
+  edges <- append(edges, c(f[e, 1], f[e, 2]))
+}
+g <- graph(edges, directed = FALSE)
+
+#APL
+path1 <- average.path.length(g, unconnected=TRUE)
+#Diameter
+diametro <- diameter(graph, directed = TRUE, unconnected = TRUE, weights = NULL)
