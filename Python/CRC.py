366fd18efbd2000923dea81900c907b4750b1a85
dianamlopes22 <31066715+dianamlopes22@users.noreply.github.com>
2018-10-21 13:54:46 +0100

    Update CRC.py

--- Python/CRC.py
+++ Python/CRC.py
@@ -4,9 +4,9 @@ import sys
 import matplotlib.pyplot as plt
 import pylab
 import networkx as nx
-import matplotlib.pyplot as plt
 import operator
 import numpy as np
+import collections
 
 G= nx.read_weighted_edgelist("../../fb_edgeList.txt", nodetype=int)
 
@@ -32,4 +32,12 @@ plt.xlabel('k', fontsize=12)
 plt.ylabel('Pk', fontsize=12)
 
 plt.loglog(distinct_degrees, y, 'o',markersize=0.75, color = '#00868B')
+plt.show()
+
+#Assortativity
+s = nx.average_neighbor_degree(G, nodes=None, weight=None)
+plt.figure()
+plt.loglog(degrees, s.values(), 'ko', markersize=0.3, color = '#00868B')
+plt.xlabel('Degree', fontsize=12)
+plt.ylabel('Average Neighbor Degree', fontsize=12)
 plt.show()
